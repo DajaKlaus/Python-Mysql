0dc30c50576ad432893e0ffa468721addd545341
DajaKlaus <91118237+DajaKlaus@users.noreply.github.com>
2023-12-22 10:38:45 +0100

    05 Finished without angular

--- 05/first-rest-api.py
+++ 05/first-rest-api.py
@@ -1,5 +1,6 @@
+import json
 import mysql.connector
-from flask import Flask
+from flask import Flask, jsonify
 
 #Connect to mysql
 mydb = mysql.connector.connect(
@@ -19,9 +20,59 @@ def hello():
 @app.route("/getAllDataInHtml")
 def getAllData():
     mycursor.execute("SELECT * FROM CLASH_ROYALE.Clash_Unit")
+    row_headers=[x[0] for x in mycursor.description]
     myresult = mycursor.fetchall()
     result = [];
     for x in myresult:
         print(x);
-        result.append(x);
-    return result
+        result.append(dict(zip(row_headers, x)))
+        print(result)
+    return json.dumps(result)
+
+@app.route("/air_transportHtml")
+def air_transport():
+    mycursor.execute("SELECT * FROM CLASH_ROYALE.Clash_Unit WHERE Transport = 'Air'")
+    row_headers=[x[0] for x in mycursor.description]
+    myresult = mycursor.fetchall()
+    result = [];
+    for x in myresult:
+        print(x);
+        result.append(dict(zip(row_headers, x)))
+        print(result)
+    return json.dumps(result)
+
+@app.route("/epic_unitsHtml")
+def epic_units():
+    mycursor.execute("SELECT * FROM CLASH_ROYALE.Clash_Unit WHERE Rarity = 'Epic'")
+    row_headers=[x[0] for x in mycursor.description]
+    myresult = mycursor.fetchall()
+    result = [];
+    for x in myresult:
+        print(x);
+        result.append(dict(zip(row_headers, x)))
+        print(result)
+    return json.dumps(result)
+
+@app.route("/speed_unitsHtml")
+def speed_units():
+    mycursor.execute("SELECT * FROM CLASH_ROYALE.Clash_Unit WHERE Speed = 'Medium (60)'")
+    row_headers=[x[0] for x in mycursor.description]
+    myresult = mycursor.fetchall()
+    result = [];
+    for x in myresult:
+        print(x);
+        result.append(dict(zip(row_headers, x)))
+        print(result)
+    return json.dumps(result)
+
+@app.route("/costHtml")
+def cost():
+    mycursor.execute("SELECT * FROM CLASH_ROYALE.Clash_Unit WHERE Cost = '4'")
+    row_headers=[x[0] for x in mycursor.description]
+    myresult = mycursor.fetchall()
+    result = [];
+    for x in myresult:
+        print(x);
+        result.append(dict(zip(row_headers, x)))
+        print(result)
+    return json.dumps(result)
